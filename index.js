eadabecbb6063e1af83dc4658612d4bc71c35d18
Marco Bettiolo <marco@bettiolo.it>
2016-02-12 14:37:12 +0000

    Added blocked to monitor when eventloop is not responding

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // This file is used to transition from ES5 to ES2017
+var warn = require('debug')('app:es5:warn');
+require('blocked')(function (ms) {
+  warn('Event loop blocked for %sms', ms | 0);
+});
 
 require('babel-register');
 require('babel-polyfill');
