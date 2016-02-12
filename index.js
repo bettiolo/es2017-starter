7bfc25015203123e8f03f3050d13139c963d85a3
Marco Bettiolo <marco@bettiolo.it>
2016-02-12 15:43:56 +0000

    Support for stage4 ES2016 proposals

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 // This file is used to transition from ES5 to ES2017
-var warn = require('debug')('app:es5:warn');
+var debug = require('debug');
+var log = debug('app:es5:warn');
+var warn = debug('app:es5:warn');
+
 require('blocked')(function (ms) {
   warn('Event loop blocked for %sms', ms | 0);
 });
 
+log('Transpiling...');
 require('babel-register');
 require('./src/index');
--- src/example.js
+++ src/example.js
@@ -1,21 +1,32 @@
 // Module Syntax + Async example
-
+import assert from 'assert';
 import debug from 'debug';
 const log = debug('app:example');
+const error = debug('app:example:error');
 
-async function asyncTimeout(delay = 100) {
-  log(`Waiting for ${delay}ms`);
+async function asyncTimeout(delay = 10) {
   return new Promise((resolve) => {
-    setTimeout(() => {
-      log(`Timeout: ${delay}ms`);
-      resolve();
-    }, delay);
+    setTimeout(() => resolve('OK'), delay);
   });
 }
 
-async function fancyFoo() {
-  await asyncTimeout(100);
-  return 'Async FOO :)';
+async function test() {
+  log('Array.prototype.includes');
+  assert.ok(['a', 'b', 'c'].includes('a'));
+
+  log('Exponentiation operator (**)');
+  assert.equal(3 ** 2, Math.pow(3, 2));
+
+  log('async functions');
+  assert.equal(await asyncTimeout(), 'OK');
 }
 
-export default () => fancyFoo().then(log);
+export default () => {
+  // Using setTimeout to free the eventloop before testing
+  setTimeout(() => {
+    log('Testing...');
+    test()
+      .then(() => log('Completed.'))
+      .catch(error);
+  }, 0);
+};
